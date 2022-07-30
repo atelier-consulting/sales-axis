e71d41372fdfadae15757d387d827c111006ed7a
Serge Tymoshenko <serge@tymo.name>
2022-07-30 09:13:10 +0300

    Apply preset

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,pug}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+  experimental: {
+    optimizeUniversalDefaults: true,
+  },
+}
