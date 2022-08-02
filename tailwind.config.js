7a4d7186063605bac7ae4e57c2fd24826b3a4b43
Serge Tymoshenko <serge@tymo.name>
2022-08-02 16:13:13 +0300

    WIP

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,27 @@ module.exports = {
   content: ["./src/**/*.{html,pug}"],
   theme: {
     extend: {
+      colors: {
+        "brand-blue": "#2a85ff",
+      },
       fontSize: {
         xxs: ".6125rem",
+      },
+      width: {
+        logo: "152px",
+      },
+      height: {
+        trend: "28.5rem",
+      },
+      textIndent: {
+        offscreen: "-6999px",
+      },
+      backgroundImage: {
+        logo: "url(\"img/logo.png\")",
+        trend: "linear-gradient(to right, rgba(0,0,0,.4), rgba(0,0,0,0)), var(--provided-img)",
+      },
+      backgroundSize: {
+        logo: "152px auto",
       },
     },
   },
