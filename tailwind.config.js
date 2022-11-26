b022315fb3eca9ad9c7d79e6562af24af1834a19
Serge Tymoshenko <serge@tymo.name>
2022-11-26 22:38:29 +0200

    WIP: Create testimonials

--- tailwind.config.js
+++ tailwind.config.js
@@ -23,10 +23,15 @@ module.exports = {
         logo: "url(\"img/logo.png\")",
         "logo-small": "url(\"img/logo.png\")",
         trend: "linear-gradient(to right, rgba(0,0,0,.4), rgba(0,0,0,0)), var(--provided-img)",
+        testimonials: "url(\"img/testimonials-bg.png\")",
       },
       backgroundSize: {
         logo: "152px auto",
         "logo-small": "86px auto",
+        testimonials: "182px auto",
+      },
+      backgroundPosition: {
+        testimonials: "top 2.24rem right 2rem",
       },
     },
   },
